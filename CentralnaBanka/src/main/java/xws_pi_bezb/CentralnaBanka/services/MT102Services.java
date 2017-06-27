8cce5b9f9c617eedfc4daa2bc23e31699c2a7474
Acasm94 <acasm94@gmail.com>
2017-06-27 22:59:35 +0200

    Dodavanje nekih stvari...

---
+++ CentralnaBanka/src/main/java/xws_pi_bezb/CentralnaBanka/services/MT102Services.java
@@ -0,0 +1,53 @@
+package xws_pi_bezb.CentralnaBanka.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import xws_pi_bezb.CentralnaBanka.irepositories.IMT102Repository;
+import xws_pi_bezb.CentralnaBanka.iservices.IMT102Services;
+import xws_pi_bezb.CentralnaBanka.models.MT102;
+
+@Service
+public class MT102Services implements IMT102Services {
+
+	@Autowired
+	private IMT102Repository repo;
+
+	@Override
+	public void save(MT102 mt102) {
+		repo.save(mt102);
+
+	}
+
+	@Override
+	public void delete(MT102 mt102) {
+		repo.delete(mt102);
+
+	}
+
+	@Override
+	public List<MT102> findAll() {
+
+		return repo.findAll();
+	}
+
+	@Override
+	public MT102 findOne(Long id) {
+
+		return repo.findById(id);
+	}
+
+	@Override
+	public MT102 findByIdPoruke(String idPorukeNaloga) {
+		// TODO Auto-generated method stub
+		return repo.findByIdPoruke(idPorukeNaloga);
+	}
+
+	@Override
+	public MT102 findBySwiftDuznikAndSwiftPoverilacAndPoslat(String swiftKod, String swiftKod2, boolean poslat) {
+		return repo.findBySwiftDuznikAndSwiftPoverilacAndPoslat(swiftKod, swiftKod2, poslat);
+	}
+
+}
--- CentralnaBanka/src/main/java/xws_pi_bezb/CentralnaBanka/webservis/CentralnaBankaEndpoint.java
+++ CentralnaBanka/src/main/java/xws_pi_bezb/CentralnaBanka/webservis/CentralnaBankaEndpoint.java
@@ -10,12 +10,18 @@ import bezbednost.poslovna.xml.ws.mt102.MT102Request;
 import bezbednost.poslovna.xml.ws.mt102.MT102Response;
 import bezbednost.poslovna.xml.ws.mt103.MT103Request;
 import bezbednost.poslovna.xml.ws.mt103.MT103Response;
+import bezbednost.poslovna.xml.ws.mt900.MT900Request;
+import xws_pi_bezb.CentralnaBanka.CentralnaBankaKlijent;
+import xws_pi_bezb.CentralnaBanka.models.MT102;
 
 @Endpoint
 public class CentralnaBankaEndpoint {
 
 	private static final String HTTP = "http://";
 	private static final String NAMESPACE_URI = "ws.xml.poslovna.bezbednost/";
+	
+	//@Autowired
+	//private 
 	
 	@Autowired
 	public CentralnaBankaEndpoint(){
@@ -26,15 +32,47 @@ public class CentralnaBankaEndpoint {
 	@ResponsePayload
 	public MT102Response mt102(@RequestPayload MT102Request request){
 		MT102Response response = new MT102Response();
-		response.setOdgovor("MT102 - odgovor");
+		
+		//TODO:A Cuvati u bazi, MT102, dodati mu oznaku boolean obradjen
+		//MT102 mt102 = konvertujMt102Request(request);
+		
+		
+		response.setOdgovor("MT102 - sve ok");
 		return response;
 	}
 	
 	@PayloadRoot(namespace = HTTP + "MT103." + NAMESPACE_URI, localPart = "MT103Request")
 	@ResponsePayload
 	public MT103Response mt103(@RequestPayload MT103Request request){
-		MT103Response response = new MT103Response();
-		response.setOdgovor("MT103 - odgovor");
+		MT103Response response = new MT103Response();	
+		CentralnaBankaKlijent klijent = new CentralnaBankaKlijent();
+		
+		// poruka o zaduzenju MT900
+		MT900Request mt900 = new MT900Request();
+		mt900.setIDPoruke("MT900");
+		mt900.setIDPorukeNaloga(request.getIDPoruke());
+		mt900.setIznos(request.getNalog().getIznos());
+		mt900.setSifraValute(request.getSifraValute());
+		mt900.setDatumValute(request.getNalog().getDatumValute());
+		mt900.setBankaDuznika(request.getBankaDuznika());
+		
+		klijent.posaljiMT900(mt900, "http://localhost:9000/ws/MT900");
+		
+		// poruka MT103 drugoj banci
+		klijent.posaljiMT103(request, "http://localhost:9090/ws/MT103");
+		
+		// poruka o odobrenju MT900
+		MT900Request mt910 = new MT900Request();
+		mt910.setIDPoruke("MT910");
+		mt910.setIDPorukeNaloga(request.getIDPoruke());
+		mt910.setIznos(request.getNalog().getIznos());
+		mt910.setSifraValute(request.getSifraValute());
+		mt910.setDatumValute(request.getNalog().getDatumValute());
+		mt910.setBankaDuznika(request.getBankaPoverioca());
+		
+		klijent.posaljiMT910(mt910, "http://localhost:9090/ws/MT910");
+		
+		response.setOdgovor("MT103 - sve ok");
 		return response;
 	}
 	
